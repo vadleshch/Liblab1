015fbcc14d49c68cef7e75eb77e7b14cc8bfc916
vladleshch <leshchenkovlad24@gmail.com>
2025-03-09 18:05:26 +0200

    comm

--- Liblab1.Test/UnitTest1.cs
+++ Liblab1.Test/UnitTest1.cs
@@ -16,41 +16,46 @@ namespace Liblab1.Test
             Assert.That(result, Is.EqualTo(1));
             result = calc.Add(-1, -1);
             Assert.That(result, Is.EqualTo(-2));
+            result = calc.Add(2^31, 2^31);
+            Assert.That(result, Is.EqualTo(2^32));
         }
+        [Test]
         public void Subtest()
         {
             var calc = new Calc();
-            var result = calc.Add(0, 0);
+            var result = calc.Sub(0, 0);
             Assert.That(result, Is.EqualTo(0));
-            result = calc.Add(1, 1);
+            result = calc.Sub(1, 1);
             Assert.That(result, Is.EqualTo(0));
-            result = calc.Add(-1, 2);
+            result = calc.Sub(-1, 2);
             Assert.That(result, Is.EqualTo(-3));
-            result = calc.Add(-1, -1);
+            result = calc.Sub(-1, -1);
             Assert.That(result, Is.EqualTo(0));
         }
+        [Test]
         public void Multtest()
         {
             var calc = new Calc();
-            var result = calc.Add(0, 0);
+            var result = calc.Mult(0, 0);
             Assert.That(result, Is.EqualTo(0));
-            result = calc.Add(1, 1);
+            result = calc.Mult(1, 1);
             Assert.That(result, Is.EqualTo(1));
-            result = calc.Add(-1, 2);
+            result = calc.Mult(-1, 2);
             Assert.That(result, Is.EqualTo(-2));
-            result = calc.Add(-1, -1);
+            result = calc.Mult(-1, -1);
             Assert.That(result, Is.EqualTo(1));
         }
+        [Test]
         public void Modtest()
         {
             var calc = new Calc();
-            var result = calc.Add(0, 0);
+            var result = calc.Mod(0, 0);
             Assert.That(result, Is.EqualTo(0));
-            result = calc.Add(1, 1);
+            result = calc.Mod(1, 1);
             Assert.That(result, Is.EqualTo(0));
-            result = calc.Add(100, 2);
+            result = calc.Mod(100, 2);
             Assert.That(result, Is.EqualTo(0));
-            result = calc.Add(101, 2);
+            result = calc.Mod(101, 2);
             Assert.That(result, Is.EqualTo(1));
         }
     }
